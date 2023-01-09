87c1beb8ff52e19d262c509a973af054fa0e7c33
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-08 21:14:00 -0300

    Log.info, RequestHeader, RequestParam e Lógica de acesso via token.

--- src/main/java/com/gonzaga/contatos/controller/ContatosController.java
+++ src/main/java/com/gonzaga/contatos/controller/ContatosController.java
@@ -41,25 +41,28 @@ public class ContatosController {
                                        /* Quando o paramêtro recebe o mesmo nome que o Header
                                         não necessidade de adicionar value */
 
-         /* Garantindo que não seja gerado um nullPointer
-         ao inves de if (!auth.equals(TOKEN_ACCESS))
-         Certificamas qual variavel / Objeto contém um valor
-         e priorizamos-lá na ordem de acesso */
+        log.info("ContatosController.cadastrar init");
 
-         var contatoCriado = contatosService.cadastrar(contato);
+        var contatoCriado = contatosService.cadastrar(contato);
 
-         if (!TOKEN_ACCESS.equals(auth)){
-             return ResponseEntity.badRequest().body("Token de Acesso Inválido");
-         }
+        if (!TOKEN_ACCESS.equals(token)) {
+             return ResponseEntity.badRequest().build();
+        }
 
         return Objects.isNull(contatoCriado) ?
                 ResponseEntity.badRequest().build() :
                 ResponseEntity.ok(contatoCriado);
     }
 
-     @GetMapping
-     public List<Contato> listar(){
-         return contatosService.listar();
+    @GetMapping
+    public ResponseEntity<List<Contato>> listar(@RequestHeader(value = "Token") String token){
+
+         log.info("ContatosController.listar init");
+
+         if (!TOKEN_ACCESS.equals(token)) {
+             return ResponseEntity.badRequest().build();
+         }
+         return ResponseEntity.ok(contatosService.listar());
      }
 
 
@@ -68,11 +71,19 @@ public class ContatosController {
     public  ResponseEntity<?> buscarPorId(@PathVariable String id,
                                           @RequestHeader(value = "Token") String token){
 
-         var resp = contatosService.buscarPorId(id);
+        log.info("ContatosController.buscarPorId init");
+        var resp = contatosService.buscarPorId(id);
 
-         // Op. Ternário: se a resposta for nula, retorne um 404 se não retorne a resposta.
-         return isNull(resp) ? ResponseEntity.status(404).build() : ResponseEntity.ok(resp);
-     }
+        if (!TOKEN_ACCESS.equals(token)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        // Op. Ternário: se a resposta for nula, retorne um 404 se não retorne a resposta.
+
+        return isNull(resp) ?
+                ResponseEntity.notFound().build() :
+                ResponseEntity.ok(resp);
+    }
 
      @PutMapping(value = "alterar/{id}")
      public ResponseEntity<Contato> alterar(@PathVariable String id,
@@ -82,9 +93,7 @@ public class ContatosController {
 
          var contato = contatosService.alterar(id);
 
-         if (!TOKEN_ACCESS.equals(auth)){
-             throw new RuntimeException("Token de Acesso Inválido");
-         }
+        log.info("ContatosController.inativar init");
 
          if (Objects.nonNull(nome)){
              contato.setNome(nome);
@@ -97,23 +106,28 @@ public class ContatosController {
      }
 
 
-     @DeleteMapping(value = "{id}")
-     @ResponseStatus(HttpStatus.ACCEPTED)
-     public ResponseEntity<Void> deletar(@PathVariable(value = "id") String id,
-                                         @RequestHeader(value = "Authorization") String auth) {
-
-         var contato = contatosService.deletar(id);
-
-         if (!TOKEN_ACCESS.equals(auth)){
-            throw new RuntimeException("Token de Acesso Inválido");
-         }
-         return ResponseEntity.noContent().build();
-     }
-
-    @PatchMapping(value = "inativar/{id}")
+    @DeleteMapping(value = "{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<?> inativar(@PathVariable String id,
-                         @RequestHeader(value = "Authorization") String auth) {
+    public ResponseEntity<Void> deletar(@PathVariable(value = "id") String id,
+                                        @RequestHeader(value = "Token") String token) {
+
+        log.info("ContatosController.deletar init");
+
+        var contato = contatosService.deletar(id);
+
+        if (!TOKEN_ACCESS.equals(token)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "atualizar/{id}")
+    public ResponseEntity<Contato> atualizar(@PathVariable String id,
+                                             @RequestHeader(value = "Token") String token,
+                                             @RequestParam(value = "nome", required = false) String nome,
+                                             @RequestParam(value = "documento", required = false) String documento) {
+
+        log.info("ContatosController.atualizar init");
 
         boolean contatoInativado = contatosService.inativar(id);
 
