40f2a7ccbb1388a8708ba0f09ff360985c3597c7
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-09 00:58:49 -0300

    Adicionando método de ativação na Interface

--- src/main/java/com/gonzaga/contatos/service/ContatosServices.java
+++ src/main/java/com/gonzaga/contatos/service/ContatosServices.java
@@ -4,12 +4,13 @@ import com.gonzaga.contatos.model.Contato;
 
 import java.util.List;
 
-public interface ContatosServicesInterface {
+public interface ContatosServices {
 
     List<Contato> listar();
     Contato cadastrar(Contato contato);
     Contato buscarPorId(String id);
     boolean inativar(String id);
+    boolean ativar(String id);
     Contato deletar(String id);
     Contato atualizar(String id, String nome, String documento);
 }
