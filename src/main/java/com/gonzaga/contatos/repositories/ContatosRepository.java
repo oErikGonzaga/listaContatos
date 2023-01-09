c6db85b1a6507eda90fb62762ba67a698d238092
gonzaga <erikgonzaga@outlook.com>
2023-01-09 14:04:56 -0300

    Metodo para buscar pelo documento.

--- src/main/java/com/gonzaga/contatos/repositories/ContatosRepository.java
+++ src/main/java/com/gonzaga/contatos/repositories/ContatosRepository.java
@@ -12,4 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ContatosRepository extends JpaRepository<Contato, String> {
 
+    Contato findFirstByDocumento(Long documento);
+
 }
