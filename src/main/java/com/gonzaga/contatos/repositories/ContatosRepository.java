dc1d20d8c8865d0b73e5c82aca4f16f93abfe05d
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-09 11:23:04 -0300

    Extendendo JpaRepository, comentários e imports

--- src/main/java/com/gonzaga/contatos/repositories/ContatosRepository.java
+++ src/main/java/com/gonzaga/contatos/repositories/ContatosRepository.java
@@ -1,4 +1,15 @@
 package com.gonzaga.contatos.repositories;
 
-public class ContatosRepository {
+import com.gonzaga.contatos.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Esta Interface extende a Interface JpaRepository.
+// Ela responsavel por montar nossas querys no Banco.
+// Em JPA Repository devemos apontar a nossa classe Model e o TIPO de nossa chave ID.
+// Devemos anota-la como um Bean do Spring, para que ela instancie esta classe com @Repository
+
+@Repository
+public interface ContatosRepository extends JpaRepository<Contato, String> {
+
 }
