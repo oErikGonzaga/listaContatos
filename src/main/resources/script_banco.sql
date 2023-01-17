6b75ddabb0a92270d0941c690ffc63ca675a41f5
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-17 18:00:04 -0300

    Script de Tabela Endereço com FK

--- src/main/resources/script_banco.sql
+++ src/main/resources/script_banco.sql
@@ -9,6 +9,21 @@ CREATE TABLE contato(
     documento   LONG            NOT NULL
 );
 
+CREATE TABLE endereco(
+    id          INT             NOT NULL    AUTO_INCREMENT  ,
+    cep         VARCHAR(8)      NOT NULL                    ,
+    logradouro  VARCHAR(150)    NOT NULL                    ,
+    complemento VARCHAR(30)     NOT NULL                    ,
+    numero      VARCHAR(150)    NOT NULL                    ,
+    cidade      VARCHAR(30)     NOT NULL                    ,
+    estado      VARCHAR(2)      NOT NULL                    ,
+    id_endereco INT             NOT NULL                    ,
+    PRIMARY KEY	(id)									    ,
+    FOREIGN KEY (address_id) REFERENCES address (id)
+);
+
+
 SELECT * FROM contato;
 
 INSERT INTO contato VALUES('12345678900', 'Erik Gonzaga de Souza', '12345678900', true);
+INSERT INTO endereco VALUES(1, '02542110', 'Rua Abura', 'A', '641', 'São Paulo', 'SP');
