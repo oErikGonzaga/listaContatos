7d43acbb37220e32e07b85f78d4abc593046ee4e
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-17 20:47:25 -0300

    Update no script_banco.sql

--- src/main/resources/script_banco.sql
+++ src/main/resources/script_banco.sql
@@ -2,28 +2,29 @@ CREATE DATABASE contatos_db;
 
 USE contatos_db;
 
-CREATE TABLE contato(
-    id          VARCHAR(255)    NOT NULL    PRIMARY KEY,
-    nome        VARCHAR(255)    NOT NULL               ,
-    ativo       BOOLEAN         NOT NULL               ,
-    documento   LONG            NOT NULL
-);
+DROP TABLE contato ;
+DROP TABLE endereco ;
 
 CREATE TABLE endereco(
-    id          INT             NOT NULL    AUTO_INCREMENT  ,
+    id          LONG            NOT NULL    AUTO_INCREMENT  ,
     cep         VARCHAR(8)      NOT NULL                    ,
     logradouro  VARCHAR(150)    NOT NULL                    ,
-    complemento VARCHAR(30)     NOT NULL                    ,
+    complemento VARCHAR(30)                                 ,
     numero      VARCHAR(150)    NOT NULL                    ,
+    bairro      VARCHAR(30)     NOT NULL                    ,
     cidade      VARCHAR(30)     NOT NULL                    ,
     estado      VARCHAR(2)      NOT NULL                    ,
-    id_endereco INT             NOT NULL                    ,
-    PRIMARY KEY	(id)									    ,
-    FOREIGN KEY (address_id) REFERENCES address (id)
+    PRIMARY KEY	(id)
 );
 
+CREATE TABLE contato(
+    id          VARCHAR(255)	NOT NULL	PRIMARY KEY		,
+    nome        VARCHAR(255)    NOT NULL                    ,
+    ativo       BOOLEAN         NOT NULL                    ,
+    documento   LONG            NOT NULL                    ,
+    id_endereco INT    			NOT NULL  					,
+    FOREIGN KEY (id_endereco)   REFERENCES  endereco (id)
+);
 
 SELECT * FROM contato;
-
-INSERT INTO contato VALUES('12345678900', 'Erik Gonzaga de Souza', '12345678900', true);
-INSERT INTO endereco VALUES(1, '02542110', 'Rua Abura', 'A', '641', 'São Paulo', 'SP');
+SELECT * FROM endereco;
