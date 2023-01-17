b66d60be60cb0358fac45dbc0f6ba54c50905989
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-17 17:58:07 -0300

    Classe Model Endereço para mapeamento no banco através de consumo de api VIACEP.

---
+++ src/main/java/com/gonzaga/contatos/model/Endereco.java
@@ -0,0 +1,22 @@
+package com.gonzaga.contatos.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "endereco")
+public class Endereco implements Serializable {
+
+    @Id
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
