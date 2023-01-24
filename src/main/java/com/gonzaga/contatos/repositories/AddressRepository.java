3d540364fbd9d5d5f43cf50d4da02c784fd2dcb4
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-24 00:34:52 -0300

    buscando todos endereços por id de contato.

--- src/main/java/com/gonzaga/contatos/repositories/AddressRepository.java
+++ src/main/java/com/gonzaga/contatos/repositories/AddressRepository.java
@@ -4,6 +4,11 @@ import com.gonzaga.contatos.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    List<Address> findAllByContactId(String idContact);
+
 }
