fca0ebb3042b24dd71b0579a604728435b8a150a
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-24 00:36:32 -0300

    Refactor Rename

--- src/main/java/com/gonzaga/contatos/models/Contact.java
+++ src/main/java/com/gonzaga/contatos/models/Contact.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "contacts")
+@Table(name = "contact")
 public class Contact implements Serializable {
 
     // Anotando os valores da classe com os nomes da tabela.
