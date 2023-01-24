a021ac05e1e5f346ae85b83238761472e54fcf3d
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-23 22:40:08 -0300

    Refactor Rename

---
+++ src/main/java/com/gonzaga/contatos/services/ContactsServices.java
@@ -0,0 +1,17 @@
+package com.gonzaga.contatos.services;
+
+import com.gonzaga.contatos.models.Contact;
+
+import java.util.List;
+
+public interface ContactsServices {
+
+    List<Contact> list(Boolean active);
+    Contact register(Contact contact);
+    Contact searchById(String id);
+    boolean inactivate(String id);
+    boolean activate(String id);
+    boolean delete(String id);
+    boolean update(String id, String name, String document);
+    List<Contact> imports(Contact contact);
+}
