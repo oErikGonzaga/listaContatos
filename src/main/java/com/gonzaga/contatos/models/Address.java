f62c18d411cc1a2a4f05c82ae037f2f922ae22cd
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-23 22:21:57 -0300

    Mapeamento class Address.

---
+++ src/main/java/com/gonzaga/contatos/models/Address.java
@@ -0,0 +1,40 @@
+package com.gonzaga.contatos.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "address")
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    @Column(length = 8, nullable = false)
+    private String cep;
+
+    @Column(length = 150, nullable = false)
+    private String address;
+
+    @Column(length = 150)
+    private String number;
+
+    @Column(length = 30)
+    private String complement;
+
+    @Column(length = 30, nullable = false)
+    private String district;
+
+    @Column(length = 30, nullable = false)
+    private String city;
+
+    @Column(length = 2, nullable = false)
+    private String uf;
+}
