18c4f7c827911f66f730e9888e37ed3ec8292ea4
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-25 00:55:36 -0300

    Update - GererationType.IDENTITY

--- src/main/java/com/gonzaga/contacts/models/Address.java
+++ src/main/java/com/gonzaga/contacts/models/Address.java
@@ -1,4 +1,4 @@
-package com.gonzaga.contatos.models;
+package com.gonzaga.contacts.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -14,28 +14,28 @@ import java.io.Serializable;
 public class Address implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(length = 8, nullable = false)
     private String cep;
 
-    @Column(length = 150, nullable = false)
+    @Column(nullable = false)
     private String address;
 
-    @Column(length = 150)
+    @Column(nullable = false)
     private String number;
 
-    @Column(length = 30)
     private String complement;
 
-    @Column(length = 30, nullable = false)
+    @Column(nullable = false)
     private String district;
 
-    @Column(length = 30, nullable = false)
+    @Column(nullable = false)
     private String city;
 
-    @Column(length = 2, nullable = false)
+    @Column(nullable = false)
     private String uf;
 
     @ManyToOne
