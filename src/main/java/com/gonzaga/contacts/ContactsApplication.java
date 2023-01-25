1541f7ff0d4bbdf6aa58beb89c37bd6caa02aeb2
Erik Gonzaga <erikgonzaga@outlook.com>
2023-01-25 01:02:20 -0300

    Refactor Rename

--- src/main/java/com/gonzaga/contacts/ContactsApplication.java
+++ src/main/java/com/gonzaga/contacts/ContactsApplication.java
@@ -1,4 +1,4 @@
-package com.gonzaga.contatos;
+package com.gonzaga.contacts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
